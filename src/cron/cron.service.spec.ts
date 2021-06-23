1556d4ea5e02ffccfc823041c74614fd2c79f263
Hung Tran <trannguyenhung011086@gmail.com>
2021-06-23 09:56:17 +0700

    update unit test

--- src/cron/cron.service.spec.ts
+++ src/cron/cron.service.spec.ts
@@ -1,20 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DatabaseModule } from 'src/database/database.module';
+import { Db } from 'mongodb';
 import { CronService } from './cron.service';
 
 describe('CronService', () => {
   let service: CronService;
+  let connection: Db;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule],
-      providers: [CronService],
+      providers: [
+        CronService,
+        {
+          provide: 'DATABASE_CONNECTION',
+          useFactory: () => ({
+            db: Db,
+            collection: jest.fn().mockReturnThis(),
+            updateMany: jest.fn().mockResolvedValue({ modifiedCount: 1 }),
+          }),
+        },
+      ],
     }).compile();
 
     service = module.get<CronService>(CronService);
+    connection = module.get('DATABASE_CONNECTION');
   });
 
-  it('should be defined', () => {
+  it('should be defined', async () => {
     expect(service).toBeDefined();
+  });
+
+  it('should confirmOrderEveryMinute', async () => {
+    await service.confirmOrderEveryMinute();
+    expect(connection.collection('orders').updateMany).toHaveBeenCalled();
   });
 });
